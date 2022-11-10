dc16eda6417ec086c255462baeede09ca0343f2d
MertCetin <mert.cetnn@gmail.com>
2022-11-09 22:17:03 -0500

    webpack manifest templated added , will edit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //DONE
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
@@ -21,6 +21,32 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "Text Editor",
+      }),
+      new InjectManifest({
+        swSrc: "./src/sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve("src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          },
+          {
+            src: path.resolve("src/assets/large-icon.png"),
+            size: "1024x1024", // you can also use the specifications pattern
+          },
+          {
+            src: path.resolve("src/assets/maskable-icon.png"),
+            size: "1024x1024",
+            purpose: "maskable",
+          },
+        ],
       }),
     ],
 
